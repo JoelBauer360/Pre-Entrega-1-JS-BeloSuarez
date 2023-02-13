361d47c1d82a236b861d43d6de910209322c8346
joelb <joelbauer360@hotmail.com>
2023-02-13 16:27:18 -0300

    Se actualizan ciclos y DOM

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let calcularPrecioFinalBtn = document.getElementById("calcularPrecioFinalBtn");
-let precioInput = document.getElementById("precio");
-let impuestosInput = document.getElementById("impuestos");
-let descuentoInput = document.getElementById("descuento");
+let inputs = [
+  document.getElementById("precio"),
+  document.getElementById("impuestos"),
+  document.getElementById("descuento"),
+];
 let precioFinal = document.getElementById("precioFinal");
 
 calcularPrecioFinalBtn.addEventListener("click", function() {
-  let precio = parseFloat(precioInput.value);
-  let impuestos = parseFloat(impuestosInput.value);
-  let descuento = parseFloat(descuentoInput.value);
-
-  if (isNaN(precio) || isNaN(impuestos) || isNaN(descuento)) {
-    alert("Por favor ingrese números válidos para el precio, los impuestos y el descuento");
-    return;
+  let values = [];
+  for (let i = 0; i < inputs.length; i++) {
+    let value = parseFloat(inputs[i].value);
+    if (isNaN(value)) {
+      alert("Por favor ingrese números válidos para el precio, los impuestos y el descuento");
+      return;
+    }
+    values.push(value);
   }
 
+  let [precio, impuestos, descuento] = values;
   let precioConImpuestos = precio + (precio * impuestos / 100);
   let precioFinalValue = precioConImpuestos - (precioConImpuestos * descuento / 100);
   precioFinal.innerHTML = "Precio final: $" + precioFinalValue.toFixed(2);
